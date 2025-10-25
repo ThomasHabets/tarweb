43132a2dc0650251e5f0e5a11aeaee0f67f854f0
Thomas Habets <thomas@habets.se>
2025-10-25 19:09:42 +0100

    Move archive handling code into its own module

---
+++ src/archive.rs
@@ -0,0 +1,193 @@
+use std::collections::HashMap;
+use std::io::Read;
+
+use crate::ENABLE_ETAGS;
+use anyhow::{Context, Result};
+use tracing::{info, trace};
+
+#[must_use]
+fn calculate_etag(bytes: &[u8]) -> String {
+    use sha3::Digest;
+    let mut h = sha3::Sha3_256::new();
+    h.update(bytes);
+    format!("{:#x}", h.finalize()).to_string()
+}
+
+/// Archive consists of the entire index and the memory mapped data.
+pub struct Archive {
+    mmap: memmap2::Mmap,
+    content: HashMap<String, ArchiveEntry>,
+}
+
+impl Archive {
+    /// Map and index an archive.
+    pub fn new<P: AsRef<std::path::Path>>(filename: P, prefix: &str) -> Result<Self> {
+        let file = std::fs::File::open(filename)?;
+        let map = unsafe { memmap2::Mmap::map(&file)? };
+        // Can't hurt to at least ask to be hugepages or mergable.
+        map.advise(memmap2::Advice::HugePage)?;
+        Self::new_inner(map, file, prefix)
+    }
+
+    /// Map and index an archive with hugepages.
+    ///
+    /// TODO: use builder pattern.
+    pub fn hugepages<P: AsRef<std::path::Path>>(
+        filename: P,
+        prefix: &str,
+        bits: u8,
+    ) -> Result<Self> {
+        use std::io::Seek;
+
+        let mut file = std::fs::File::open(filename)?;
+        let len = file.metadata()?.len();
+
+        let page_size = 1 << bits;
+        let maplen = (len + (page_size - 1)) & !(page_size - 1);
+        let mut m = memmap2::MmapOptions::new()
+            .len(maplen as usize)
+            .huge(Some(bits))
+            .map_anon()
+            .with_context(|| format!("Failed allocating 1<<{bits}={} bytes", 1 << bits))?;
+
+        // Rewind the file.
+        file.read_exact(&mut m.as_mut()[..len.try_into()?])?;
+        file.seek(std::io::SeekFrom::Start(0))?;
+        Self::new_inner(m.make_read_only()?, file, prefix)
+    }
+
+    /// Shared constructor code.
+    fn new_inner(mmap: memmap2::Mmap, file: std::fs::File, prefix: &str) -> Result<Self> {
+        mmap.advise(memmap2::Advice::Mergeable)?;
+        let mut archive = tar::Archive::new(&file);
+        let mut content = HashMap::new();
+        info!("Indexing…");
+        for e in archive.entries()? {
+            let e = e?;
+            if let tar::EntryType::Regular = e.header().entry_type() {
+            } else {
+                continue;
+            }
+            let name = e.path()?;
+            let name = name.to_string_lossy();
+            let name = name.strip_prefix(prefix).unwrap_or(name.as_ref());
+            content.insert(
+                name.to_string(),
+                ArchiveEntry {
+                    plain: ArchiveEntry2 {
+                        pos: e.raw_file_position() as usize,
+                        len: e.size() as usize,
+                    },
+                    brotli: None,
+                    gzip: None,
+                    zstd: None,
+                    modified: e
+                        .header()
+                        .mtime()
+                        .map(|t| std::time::UNIX_EPOCH + std::time::Duration::from_secs(t))
+                        .ok(),
+                    etag: None, // Set later.
+                },
+            );
+        }
+        if ENABLE_ETAGS {
+            info!("Hashing etags…");
+            use rayon::iter::IntoParallelRefMutIterator;
+            use rayon::iter::ParallelIterator;
+            content.par_iter_mut().for_each(|(_k, v)| {
+                v.etag = Some(calculate_etag(
+                    &mmap[v.plain.pos..(v.plain.pos + v.plain.len)],
+                ));
+            });
+        }
+        let c2 = content.clone();
+        for (k, v) in c2.iter() {
+            if k.ends_with(".br") {
+                let k = &k[..(k.len() - 3)];
+                if let Some(r) = content.get_mut(k) {
+                    r.brotli = Some(v.plain.clone());
+                }
+            }
+            if k.ends_with(".zstd") {
+                let k = &k[..(k.len() - 5)];
+                if let Some(r) = content.get_mut(k) {
+                    r.zstd = Some(v.plain.clone());
+                }
+            }
+            if k.ends_with(".gz") {
+                let k = &k[..(k.len() - 3)];
+                if let Some(r) = content.get_mut(k) {
+                    r.gzip = Some(v.plain.clone());
+                }
+            }
+        }
+        Ok(Self { content, mmap })
+    }
+
+    /// Get the ArchiveEntry for a given filename.
+    #[must_use]
+    pub fn entry(&self, filename: &str) -> Option<&ArchiveEntry> {
+        use std::borrow::Cow;
+        if filename.is_empty() {
+            return None;
+        }
+        // Strip initial slash.
+        let filename = filename.strip_prefix("/").unwrap_or(filename);
+
+        // Add index.html to directory paths.
+        let filename = if filename.is_empty() || filename.ends_with('/') {
+            Cow::Owned(filename.to_owned() + "index.html")
+        } else {
+            Cow::Borrowed(filename)
+        };
+
+        trace!("Looking up {filename}");
+        self.content.get(filename.as_ref())
+    }
+
+    /// Get a slice from the mapped memory area.
+    #[must_use]
+    pub fn get_slice(&self, pos: usize, len: usize) -> &[u8] {
+        let data: &[u8] = &self.mmap;
+        &data[pos..(pos + len)]
+    }
+}
+
+/// Just the byte range inside the mapped area.
+#[derive(Clone, Debug)]
+pub struct ArchiveEntry2 {
+    pub pos: usize,
+    pub len: usize,
+}
+
+/// All metadata about a particular file, including alternative encodings.
+#[derive(Clone)]
+pub struct ArchiveEntry {
+    plain: ArchiveEntry2,
+    gzip: Option<ArchiveEntry2>,
+    brotli: Option<ArchiveEntry2>,
+    zstd: Option<ArchiveEntry2>,
+    modified: Option<std::time::SystemTime>,
+    etag: Option<String>,
+}
+
+impl ArchiveEntry {
+    pub fn plain(&self) -> &ArchiveEntry2 {
+        &self.plain
+    }
+    pub fn brotli(&self) -> Option<&ArchiveEntry2> {
+        self.brotli.as_ref()
+    }
+    pub fn gzip(&self) -> Option<&ArchiveEntry2> {
+        self.gzip.as_ref()
+    }
+    pub fn zstd(&self) -> Option<&ArchiveEntry2> {
+        self.zstd.as_ref()
+    }
+    pub fn modified(&self) -> Option<&std::time::SystemTime> {
+        self.modified.as_ref()
+    }
+    pub fn etag(&self) -> Option<&str> {
+        self.etag.as_deref()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,6 @@
 // * --accept-multi=false (otherwise all goes to first thread)
 // * --cpu-affinity=false (not sure why, but CPU affinity hurts a lot)
 
-use std::collections::HashMap;
 use std::io::Read;
 use std::os::fd::AsRawFd;
 use std::pin::Pin;
@@ -40,11 +39,14 @@ use tracing::{debug, error, info, trace, warn};
 
 use rustls::pki_types::{CertificateDer, PrivateKeyDer};
 
+mod archive;
+use archive::Archive;
+
 type FixedFile = io_uring::types::Fixed;
 
 // Enable etags for caching. Slows down startup, since we need to hash all
 // files.
-const ENABLE_ETAGS: bool = true;
+pub const ENABLE_ETAGS: bool = true;
 
 // Cache max age.
 const CACHE_AGE_SECS: u64 = 300;
@@ -118,14 +120,6 @@ const MAX_HEADER_BUF: usize = 1024;
 type SQueue = ArrayVec<io_uring::squeue::Entry, 10_000>;
 type ReadBuf = [u8; MAX_READ_BUF];
 type HeaderBuf = ArrayVec<u8, MAX_HEADER_BUF>;
-
-#[must_use]
-fn calculate_etag(bytes: &[u8]) -> String {
-    use sha3::Digest;
-    let mut h = sha3::Sha3_256::new();
-    h.update(bytes);
-    format!("{:#x}", h.finalize()).to_string()
-}
 
 // State only needed during handshake.
 //
@@ -958,23 +952,23 @@ fn maybe_answer_req(hook: &mut Hook, ops: &mut SQueue, archive: &Archive) -> Res
         // TODO: actually, go with the smallest option. My experience just is
         // that it's always this order.
         let (subentry, encoding) = if req.encoding_brotli
-            && let Some(ref e) = entry.brotli
+            && let Some(e) = entry.brotli()
         {
             (e, "Content-Encoding: br\r\n")
         } else if req.encoding_zstd
-            && let Some(ref e) = entry.zstd
+            && let Some(e) = entry.zstd()
         {
             (e, "Content-Encoding: zstd\r\n")
         } else if req.encoding_gzip
-            && let Some(ref e) = entry.gzip
+            && let Some(e) = entry.gzip()
         {
             (e, "Content-Encoding: gzip\r\n")
         } else {
-            (&entry.plain, "")
+            (entry.plain(), "")
         };
         // TODO: pre-calculate many of these headers.
-        let mtime = entry.modified.map_or("".to_string(), |mtime| {
-            format!("Last-Modified: {}\r\n", httpdate::fmt_http_date(mtime))
+        let mtime = entry.modified().map_or("".to_string(), |mtime| {
+            format!("Last-Modified: {}\r\n", httpdate::fmt_http_date(*mtime))
         });
         let caching = if CACHE_AGE_SECS > 0 {
             // Expires header is ignored when providing max-age.
@@ -982,7 +976,7 @@ fn maybe_answer_req(hook: &mut Hook, ops: &mut SQueue, archive: &Archive) -> Res
         } else {
             ""
         };
-        let etag = if let Some(e) = &entry.etag {
+        let etag = if let Some(e) = entry.etag() {
             &format!("ETag: {e}\r\n")
         } else {
             ""
@@ -992,9 +986,9 @@ fn maybe_answer_req(hook: &mut Hook, ops: &mut SQueue, archive: &Archive) -> Res
             httpdate::fmt_http_date(std::time::SystemTime::now()),
         );
 
-        if req.if_modified_since.zip(entry.modified).is_some_and(|(h,e)| e <= h)
+        if req.if_modified_since.zip(entry.modified()).is_some_and(|(h,e)| *e <= h)
             // Split can only be iterated once, hence mut here.
-            || req.if_none_match.zip(entry.etag.as_ref()).is_some_and(|(mut h,e)| {
+            || req.if_none_match.zip(entry.etag()).is_some_and(|(mut h,e)| {
                 h.any(|x| x.trim() == e)
             })
         {
@@ -1637,147 +1631,6 @@ fn parse_duration(time_str: &str) -> Result<std::time::Duration, String> {
     } else {
         Err("Invalid format. Use 'Xs' or 'Yms' (e.g., '1.5s', '500ms')".to_string())
     }
-}
-
-struct Archive {
-    mmap: memmap2::Mmap,
-    content: HashMap<String, ArchiveEntry>,
-}
-
-impl Archive {
-    fn new<P: AsRef<std::path::Path>>(filename: P, prefix: &str) -> Result<Self> {
-        let file = std::fs::File::open(filename)?;
-        let map = unsafe { memmap2::Mmap::map(&file)? };
-        // Can't hurt to at least ask to be hugepages or mergable.
-        map.advise(memmap2::Advice::HugePage)?;
-        Self::new_inner(map, file, prefix)
-    }
-    fn hugepages<P: AsRef<std::path::Path>>(filename: P, prefix: &str, bits: u8) -> Result<Self> {
-        use std::io::Seek;
-
-        let mut file = std::fs::File::open(filename)?;
-        let len = file.metadata()?.len();
-
-        let page_size = 1 << bits;
-        let maplen = (len + (page_size - 1)) & !(page_size - 1);
-        let mut m = memmap2::MmapOptions::new()
-            .len(maplen as usize)
-            .huge(Some(bits))
-            .map_anon()
-            .with_context(|| format!("Failed allocating 1<<{bits}={} bytes", 1 << bits))?;
-
-        // Rewind the file.
-        file.read_exact(&mut m.as_mut()[..len.try_into()?])?;
-        file.seek(std::io::SeekFrom::Start(0))?;
-        Self::new_inner(m.make_read_only()?, file, prefix)
-    }
-
-    fn new_inner(mmap: memmap2::Mmap, file: std::fs::File, prefix: &str) -> Result<Self> {
-        mmap.advise(memmap2::Advice::Mergeable)?;
-        let mut archive = tar::Archive::new(&file);
-        let mut content = HashMap::new();
-        info!("Indexing…");
-        for e in archive.entries()? {
-            let e = e?;
-            if let tar::EntryType::Regular = e.header().entry_type() {
-            } else {
-                continue;
-            }
-            let name = e.path()?;
-            let name = name.to_string_lossy();
-            let name = name.strip_prefix(prefix).unwrap_or(name.as_ref());
-            content.insert(
-                name.to_string(),
-                ArchiveEntry {
-                    plain: ArchiveEntry2 {
-                        pos: e.raw_file_position() as usize,
-                        len: e.size() as usize,
-                    },
-                    brotli: None,
-                    gzip: None,
-                    zstd: None,
-                    modified: e
-                        .header()
-                        .mtime()
-                        .map(|t| std::time::UNIX_EPOCH + std::time::Duration::from_secs(t))
-                        .ok(),
-                    etag: None, // Set later.
-                },
-            );
-        }
-        if ENABLE_ETAGS {
-            info!("Hashing etags…");
-            use rayon::iter::IntoParallelRefMutIterator;
-            use rayon::iter::ParallelIterator;
-            content.par_iter_mut().for_each(|(_k, v)| {
-                v.etag = Some(calculate_etag(
-                    &mmap[v.plain.pos..(v.plain.pos + v.plain.len)],
-                ));
-            });
-        }
-        let c2 = content.clone();
-        for (k, v) in c2.iter() {
-            if k.ends_with(".br") {
-                let k = &k[..(k.len() - 3)];
-                if let Some(r) = content.get_mut(k) {
-                    r.brotli = Some(v.plain.clone());
-                }
-            }
-            if k.ends_with(".zstd") {
-                let k = &k[..(k.len() - 5)];
-                if let Some(r) = content.get_mut(k) {
-                    r.zstd = Some(v.plain.clone());
-                }
-            }
-            if k.ends_with(".gz") {
-                let k = &k[..(k.len() - 3)];
-                if let Some(r) = content.get_mut(k) {
-                    r.gzip = Some(v.plain.clone());
-                }
-            }
-        }
-        Ok(Self { content, mmap })
-    }
-    #[must_use]
-    fn entry(&self, filename: &str) -> Option<&ArchiveEntry> {
-        use std::borrow::Cow;
-        if filename.is_empty() {
-            return None;
-        }
-        // Strip initial slash.
-        let filename = filename.strip_prefix("/").unwrap_or(filename);
-
-        // Add index.html to directory paths.
-        let filename = if filename.is_empty() || filename.ends_with('/') {
-            Cow::Owned(filename.to_owned() + "index.html")
-        } else {
-            Cow::Borrowed(filename)
-        };
-
-        trace!("Looking up {filename}");
-        self.content.get(filename.as_ref())
-    }
-    #[must_use]
-    fn get_slice(&self, pos: usize, len: usize) -> &[u8] {
-        let data: &[u8] = &self.mmap;
-        &data[pos..(pos + len)]
-    }
-}
-
-#[derive(Clone, Debug)]
-struct ArchiveEntry2 {
-    pos: usize,
-    len: usize,
-}
-
-#[derive(Clone)]
-struct ArchiveEntry {
-    plain: ArchiveEntry2,
-    gzip: Option<ArchiveEntry2>,
-    brotli: Option<ArchiveEntry2>,
-    zstd: Option<ArchiveEntry2>,
-    modified: Option<std::time::SystemTime>,
-    etag: Option<String>,
 }
 
 fn is_setsockopt_supported() -> Result<bool> {
