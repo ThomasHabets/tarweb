963b06332cb8ba1aa17c31e63148eb7674e1a36b
Thomas Habets <thomas@habets.se>
2025-11-13 21:13:58 +0000

    Change default crypto provider, only really needed for tests

--- src/main.rs
+++ src/main.rs
@@ -1592,7 +1592,7 @@ fn mainloop(
     archive: &Archive,
 ) -> Result<()> {
     info!("Thread main");
-    rustls::crypto::ring::default_provider()
+    rustls::crypto::aws_lc_rs::default_provider()
         .install_default()
         .unwrap();
     let mut pooltracker = PoolTracker::new(opt.max_connections);
