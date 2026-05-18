d160c36c8ae636352335532a66131e619218668b
Thomas Habets <thomas@habets.se>
2026-05-18 09:18:30 +0100

    sni: Detect EOF during handshake

--- src/bin/sni.rs
+++ src/bin/sni.rs
@@ -578,6 +578,13 @@ async fn tls_handshake(
         // Handshake still going.
         let mut buf = [0u8; 4096];
         let n = stream.read(&mut buf).await?;
+        if n == 0 {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::UnexpectedEof,
+                "EOF during handshake",
+            )
+            .into());
+        }
         bytes.extend(&buf[..n]);
 
         // TODO: what should this magic value be?
